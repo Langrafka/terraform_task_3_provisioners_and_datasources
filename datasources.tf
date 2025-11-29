01d53e449beaf2dddc621098125c5880ba2527b1
Yaroslava <lanhraf.yaroslava@gmail.com>
2025-11-29 14:04:20 +0200

    task3

--- datasources.tf
+++ datasources.tf
@@ -4,16 +4,32 @@ data "azurerm_resource_group" "existing" {
 }
 
 # 2. DATASOURCE: Отримання інформації про існуючий Virtual Network (VNet)
-# ЗМІНА: Раніше був resource, тепер data
 data "azurerm_virtual_network" "main" {
   name                = "${var.prefix}-vnet"
   resource_group_name = data.azurerm_resource_group.existing.name
 }
 
 # 3. DATASOURCE: Отримання інформації про існуючий Subnet
-# ЗМІНА: Раніше був resource, тепер data
 data "azurerm_subnet" "internal" {
   name                 = "internal"
   virtual_network_name = data.azurerm_virtual_network.main.name
   resource_group_name  = data.azurerm_resource_group.existing.name
+}
+
+# 4. DATASOURCE: Отримання інформації про існуючий Public IP
+data "azurerm_public_ip" "main" {
+  name                = "${var.prefix}-pip"
+  resource_group_name = data.azurerm_resource_group.existing.name
+}
+
+# 5. DATASOURCE: Отримання інформації про існуючий Network Interface Card (NIC)
+data "azurerm_network_interface" "main" {
+  name                = "${var.prefix}-nic"
+  resource_group_name = data.azurerm_resource_group.existing.name
+}
+
+# 6. DATASOURCE: Отримання інформації про існуючу Virtual Machine (VM)
+data "azurerm_virtual_machine" "main" {
+  name                = "${var.prefix}-vm"
+  resource_group_name = data.azurerm_resource_group.existing.name
 }
--- vm.tf
+++ vm.tf
@@ -1,99 +1,9 @@
-# 1. PUBLIC IP: Створення публічної IP-адреси
-resource "azurerm_public_ip" "main" {
-  name                = "${var.prefix}-pip"
-  location            = data.azurerm_resource_group.existing.location
-  resource_group_name = data.azurerm_resource_group.existing.name
-  allocation_method   = "Static"
-  # ВИПРАВЛЕННЯ: Рекомендовано Standard
-  sku = "Standard"
-}
-
-# 2. NETWORK SECURITY GROUP (NSG): Відкриваємо порти 22 (SSH) та 80 (HTTP)
-resource "azurerm_network_security_group" "main" {
-  name                = "${var.prefix}-nsg"
-  location            = data.azurerm_resource_group.existing.location
-  resource_group_name = data.azurerm_resource_group.existing.name
-
-  security_rule {
-    name                       = "SSH"
-    priority                   = 100
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "22"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-
-  security_rule {
-    name                       = "HTTP"
-    priority                   = 101
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "80"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-}
-
-# 3. NETWORK INTERFACE CARD (NIC)
-resource "azurerm_network_interface" "main" {
-  name                = "${var.prefix}-nic"
-  location            = data.azurerm_resource_group.existing.location
-  resource_group_name = data.azurerm_resource_group.existing.name
-
-  ip_configuration {
-    name = "testconfiguration1"
-    # ЗМІНА: Використовуємо subnet_id з data-блоку
-    subnet_id                     = data.azurerm_subnet.internal.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.main.id
-  }
-}
-
-# 4. Прив'язка NSG до NIC
-resource "azurerm_network_interface_security_group_association" "main" {
-  network_interface_id      = azurerm_network_interface.main.id
-  network_security_group_id = azurerm_network_security_group.main.id
-}
-
-# 5. VIRTUAL MACHINE (VM)
-resource "azurerm_virtual_machine" "main" {
-  name                  = "${var.prefix}-vm"
-  location              = data.azurerm_resource_group.existing.location
-  resource_group_name   = data.azurerm_resource_group.existing.name
-  network_interface_ids = [azurerm_network_interface.main.id]
-  vm_size               = "Standard_B1s"
-
-  storage_image_reference {
-    publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts"
-    version   = "latest"
-  }
-
-  storage_os_disk {
-    name              = "myosdisk1"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
-  }
-
-  os_profile {
-    computer_name  = "hostname"
-    admin_username = var.admin_username
-    admin_password = var.admin_password
-  }
-
-  os_profile_linux_config {
-    disable_password_authentication = false
-  }
-
-  tags = {
-    environment = "staging"
+# Використовуємо null_resource для виконання provisioners
+# Provisioners не можуть бути прикріплені до Data Sources (VM), тому використовуємо цей підхід.
+resource "null_resource" "nginx_config" {
+  # Trigger змушує null_resource виконувати provisioners при зміні ID VM
+  triggers = {
+    vm_id = data.azurerm_virtual_machine.main.id
   }
 
   # 1. FILE PROVISIONER: Копіює локальний index.html
@@ -105,7 +15,8 @@ resource "azurerm_virtual_machine" "main" {
       type     = "ssh"
       user     = var.admin_username
       password = var.admin_password
-      host     = azurerm_public_ip.main.ip_address
+      # Використовуємо IP-адресу, отриману з Data Source Public IP
+      host     = data.azurerm_public_ip.main.ip_address 
       timeout  = "5m"
     }
   }
@@ -116,15 +27,15 @@ resource "azurerm_virtual_machine" "main" {
       type     = "ssh"
       user     = var.admin_username
       password = var.admin_password
-      host     = azurerm_public_ip.main.ip_address
+      host     = data.azurerm_public_ip.main.ip_address
       timeout  = "5m"
     }
 
     inline = [
       "sudo apt-get update -y",
       "sudo apt-get install -y nginx",
-      # ВИПРАВЛЕННЯ: Змінено шлях з index.nginx-debian.html на index.html
-      "sudo cp /tmp/index.html /var/www/html/index.html",
+      # Виправлення шляху на /var/www/html/index.html (вимога ментора)
+      "sudo cp /tmp/index.html /var/www/html/index.html", 
       "sudo systemctl restart nginx",
     ]
   }
@@ -133,5 +44,6 @@ resource "azurerm_virtual_machine" "main" {
 # OUTPUT: Виводить публічну IP-адресу для перевірки
 output "nginx_public_ip" {
   description = "Публічна IP-адреса для доступу до веб-сервера Nginx"
-  value       = azurerm_public_ip.main.ip_address
+  # Використовуємо Data Source Public IP
+  value       = data.azurerm_public_ip.main.ip_address
 }
